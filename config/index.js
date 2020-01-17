a0b7239b130b4039da45f304cd41c4440aed51c8
lemon-Boy <59724701+lemon-Boy@users.noreply.github.com>
2020-01-17 09:20:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    // 跨域设置
+    proxyTable: {
+      '/api/**': {
+        target: '', // 这个是你要代理的地址(开发阶段接口地址)
+        changeOrigin: true,//跨域需要加上这个
+        pathRewrite: {
+          '^/api': '/' //这里的/其实就是代表根,可以理解为用/api代表target里的地址
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
