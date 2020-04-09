420187d2e9e0ccd45ea08e82a47d2d33187db36a
lemon-Boy <59724701+lemon-Boy@users.noreply.github.com>
2020-04-09 13:09:41 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import axios from 'axios'
-import 'babel-polyfill'
-import apiConfig from '../config/api.config'
 // import ElementUI from 'element-ui'
 // 全局使用ElementUI
 // import 'element-ui/lib/theme-chalk/index.css'
 // Vue.use(ElementUI)
-Vue.prototype.$ajax = axios // axios请求接口
-axios.defaults.baseURL = apiConfig.baseUrl // 跨域初始化前缀地址
+import axios from 'axios'
+import apiConfig from '../config/api.config'
+// 全局注册axios（this.axios）
+Vue.prototype.axios = axios
+axios.defaults.baseURL = apiConfig.baseUrl
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
